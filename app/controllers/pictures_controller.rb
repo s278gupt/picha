76d14c43814c055355f8688b5b812179daabff83
s278gupt <s278gupt@edu.uwaterloo.ca>
2019-10-14 19:18:29 -0400

    untracked files welp

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,53 @@
+class PicturesController < ApplicationController
+    before_action :set_picture, only: [:destroy, :edit, :update]
+
+    # GET pictures/new
+    def new
+        @picture = Picture.new
+    end
+
+    # POST pictures/new
+    def create
+        @picture = Picture.new(picture_params)
+
+        respond_to do |format|
+            if @picture.save
+                format.html { redirect_to pictures_path, notice: 'Picture was successfully uploaded.' }
+                format.json { render :index, status: :created, location: @picture }
+            else
+                format.html { render :new }
+                format.json { render json: @picture.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    # GET pictures/:id
+    def index
+        if user_signed_in?
+            @pictures = current_user.pictures.order(created_at: :desc)
+        end
+    end
+
+    # DELETE pictures/:id
+    def destroy
+        @picture.destroy
+
+        respond_to do |format|
+            format.html { redirect_to pictures_path, notice: 'Picture was successfully deleted.' }
+            format.json { head :no_content }
+        end
+    end
+
+    # GET pictures/:id/edit
+    def edit
+    end
+
+    private
+        def set_picture
+            @picture = current_user.pictures.find(params[:id])   
+        end
+
+        def picture_params
+            params.require(:picture).permit(:user_id, :image)
+        end
+end
---
+++ app/models/picture.rb
@@ -0,0 +1,19 @@
+class Picture < ApplicationRecord
+    belongs_to :user
+
+    has_one_attached :image
+
+    validate :image_type
+
+    private
+    def image_type
+
+        if !image.attached?
+            errors.add(:image, "please select images")
+        else
+            if !image.content_type.in?(%('image/jpeg' 'image/png'))
+                errors.add(:image, 'needs to be a JPEG or PNG')
+            end
+        end
+    end
+end
